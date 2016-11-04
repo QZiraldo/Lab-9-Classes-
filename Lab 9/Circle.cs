a43ea4d878a94c6623a85a4d1dec7ac80eb2e9ed
zuralldoe <quirico@gmail.com>
2016-11-04 14:05:10 -0400

    first commit

---
+++ Lab 9/Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9
+{
+    class Circle
+    {
+        private double Radius;
+
+
+
+
+        public Circle(double rad)
+        {
+
+            Radius = rad;
+        }
+
+        public double getCircumference()
+        {
+            double pi = Math.PI;
+            double Circumference = 0;
+            Circumference = (2 * pi * Radius);
+            return Circumference;
+
+        }
+
+        public double getArea()
+        {
+            double pi = Math.PI;
+            double area = 0;
+            area = (Math.Pow((pi * Radius),2));
+            return area;
+
+        }
+
+        private String formatNumber(double X)
+        {
+           double str = Math.Round(X, 2);
+            string formatX = str.ToString();
+            return formatX;
+        }
+
+        public String getFormattedCircumference()
+        {
+           return formatNumber(getCircumference());
+        }
+
+        public String getFormattedArea()
+        {
+           return formatNumber(getArea());
+        }
+
+    }
+}
